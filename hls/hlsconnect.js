785d967b7dbb157d0cb43f6b1af7a3e8fad419da
ZohebUddin <zoheb.uddin@enseirb-matmeca.fr>
2020-11-30 22:32:40 +0100

    HLS new video

--- hls/hlsconnect.js
+++ hls/hlsconnect.js
@@ -10,7 +10,7 @@ fs.stat('test360/360p.m3u8', function(err) {
   }
   else if (err.code === 'ENOENT') {
       console.log('file or directory does not exist');
-      child = exec('  ffmpeg -hide_banner -y -i data/out.mp4 \
+      child = exec('  ffmpeg -hide_banner -y -i data/bbb.mp4 \
       -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod  -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename test360/360p_%03d.ts test360/360p.m3u8 \
       -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename test480/480p_%03d.ts test480/480p.m3u8 \
       -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 2800k -maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename test720/720p_%03d.ts test720/720p.m3u8',
@@ -32,7 +32,7 @@ app.use(express.static('./'));
 
 //Create a route for the first http connection
 app.get('/', function (request, response) {
-    response.sendFile(__dirname + '/HLSPage.html');
+    response.sendFile(__dirname + '/hls.html');
     console.log(request.url);
     convert(request,response);
 
